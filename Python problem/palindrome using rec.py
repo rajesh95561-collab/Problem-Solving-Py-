adc06677e7adce9b778cd0295dc4c0e38a3ea2fa
Rajesh Kumar Sahoo <237406716+rajesh95561-collab@users.noreply.github.com>
2026-03-13 17:53:08 +0530

    Update palindrome using rec.py

--- Python problem/palindrome using rec.py
+++ Python problem/palindrome using rec.py
@@ -1,12 +1,13 @@
 # WAP on string to check string is palindrome or not
 
 def palindrome(string):
-    if len(string) == 1:
+    if len(string) <= 1:
         print("the string is palindrome")
     else:
         if string[0] == string[-1]:
             palindrome(string[1:-1])
         else:
             print("the string is not palindrome")
+
 
 palindrome("mam")
