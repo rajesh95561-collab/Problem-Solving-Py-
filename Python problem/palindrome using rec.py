40fe04f794041d429297fa1d02e864dcd3817dbe
Rajesh Kumar Sahoo <237406716+rajesh95561-collab@users.noreply.github.com>
2026-03-13 17:50:49 +0530

    Add files via upload

---
+++ Python problem/palindrome using rec.py
@@ -0,0 +1,12 @@
+# WAP on string to check string is palindrome or not
+
+def palindrome(string):
+    if len(string) == 1:
+        print("the string is palindrome")
+    else:
+        if string[0] == string[-1]:
+            palindrome(string[1:-1])
+        else:
+            print("the string is not palindrome")
+
+palindrome("mam")
