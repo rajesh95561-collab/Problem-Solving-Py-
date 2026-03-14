ee63442e75ea968a94c8e4401f9bd92368a20529
Rajesh Kumar Sahoo <237406716+rajesh95561-collab@users.noreply.github.com>
2026-03-14 17:39:29 +0530

    Update The rabbit Problem.py

--- Python problem/The rabbit Problem.py
+++ Python problem/The rabbit Problem.py
@@ -11,3 +11,8 @@ def rabbit(m):
     else:
         return rabbit(m-1)+rabbit(m-2)
 print(rabbit(12))
+
+# Recursive Fibonacci is inefficient because it repeatedly recalculates the same values, leading to exponential growth in the number of function calls. For example,
+# to compute fib(5), the program ends up computing fib(3) and fib(2) multiple times, creating a large recursion tree with overlapping subproblems.
+# This results in a time complexity of O(2^n), which becomes very slow as n increases. In contrast, iterative methods or recursion with memoization avoid this
+# duplication by storing previously computed results, reducing the complexity to O(n) or even O(\log n) with advanced techniques.
