34e93215b4e8a1bbe881d3950b3c607aab7d46ae
Rajesh Kumar Sahoo <237406716+rajesh95561-collab@users.noreply.github.com>
2026-03-19 19:09:37 +0530

    Update pass by reference.py

--- Python problem/pass by reference.py
+++ Python problem/pass by reference.py
@@ -9,7 +9,7 @@ class Customer:
         self._gender = gender
 
 def greet(customer):
-    # Modify the object inside the function
+    print(id(customer))  #memory add - 1790055435296(customer is the ref of the cust)
     customer._name = customer._name.upper()
     if customer._gender.lower() == "male":
         print(f"Hii {customer._name} sir")
@@ -17,4 +17,5 @@ def greet(customer):
         print(f"Hii {customer._name} ma'am")
 
 cust = Customer("Rajeswari", "female")
+print(id(cust))   #1790055435296
 greet(cust)
