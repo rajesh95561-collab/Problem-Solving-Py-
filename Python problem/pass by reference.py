80006cdd86b5477f7d6d58d325ece6d41b35b9a0
Rajesh Kumar Sahoo <237406716+rajesh95561-collab@users.noreply.github.com>
2026-03-19 19:03:46 +0530

    Add files via upload

---
+++ Python problem/pass by reference.py
@@ -0,0 +1,20 @@
+# Program Documentation
+# This program defines a Customer class with attributes for name and gender. A separate greet function takes a Customer object and prints a personalized greeting
+# based on gender. The logic uses case-insensitive comparison to ensure consistent results. It demonstrates Python’s object-oriented design and pass-by-reference
+# behavior when passing class instances to functions.
+
+class Customer:
+    def __init__(self, name, gender):
+        self._name = name
+        self._gender = gender
+
+def greet(customer):
+    # Modify the object inside the function
+    customer._name = customer._name.upper()
+    if customer._gender.lower() == "male":
+        print(f"Hii {customer._name} sir")
+    else:
+        print(f"Hii {customer._name} ma'am")
+
+cust = Customer("Rajeswari", "female")
+greet(cust)
