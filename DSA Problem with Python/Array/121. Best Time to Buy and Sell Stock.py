ab2c0e5579760ff1a7f4d4fd9c349644d32fdb6d
Rajesh Kumar Sahoo <237406716+rajesh95561-collab@users.noreply.github.com>
2026-04-20 17:45:05 +0530

    Create 121. Best Time to Buy and Sell Stock.py

---
+++ DSA Problem with Python/Array/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        best_buy = prices[0]
+        for i in range(1,len(prices)):
+            if best_buy < prices[i]:
+                max_profit = max(max_profit,prices[i]-best_buy)
+            best_buy = min(best_buy,prices[i])
+        return max_profit
